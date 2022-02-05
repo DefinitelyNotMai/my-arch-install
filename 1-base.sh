726222764e21d69b592f8eae00cdadf582d71441
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2022-02-06 05:11:26 +0800

    Added reflector to generate mirrorlist, made script more readable, and added prompt for my personal files

--- 1-base.sh
+++ 1-base.sh
@@ -1,67 +1,75 @@
-#!/bin/sh
+#!/bin/bash
 
-# Keyring
-pacman -Sy --noconfirm archlinux-keyring
+# install archlinux-keyring and reflector, and use reflector to generate mirrorlist
+reflector --latest 5 --sort rate --protocol https --country Germany --save /etc/pacman.d/mirrorlist --download-timeout 10
 
-# ParallelDownloads = 15 and enable multilib in pacman.conf
+# set ParallellDownloads to 15 and enable multilib repositories
 sed -i 's/^#ParallelDownloads = 5$/ParallelDownloads = 15/' /etc/pacman.conf
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
-# Use all cores for compilation and compression
+# use all cores for compilation and compression
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $(nproc) -z -)/g" /etc/makepkg.conf
 
-# Make a 8GB swapfile and set swappiness to 1
-dd if=/dev/zero of=/etc/swapfile bs=1M count=8192 status=progress
+# make an 10GB swapfile and set swappiness to 1
+dd if=/dev/zero of=/etc/swapfile bs=1M count=10240 status=progress
 chmod 600 /etc/swapfile
 mkswap /etc/swapfile
 swapon /etc/swapfile
-echo "/etc/swapfile none swap defaults 0 0" >> /etc/fstab
-echo "vm.swappiness=1" >> /etc/sysctl.d/99-swappiness.conf
+printf "/etc/swapfile none swap defaults 0 0" >> /etc/fstab
+printf "vm.swappiness=1" >> /etc/sysctl.d/99-swappiness.conf
 
-# Locale, Host, Root password
-ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
+# set locale, hostname and hosts, and set root password
+ln -sf /usr/share/zoneinfo/Asia/Manila /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+printf "LANG=en_US.UTF-8" >> /etc/locale.conf
 read -p "Enter desired hostname: " hsn
-echo "$hsn" >> /etc/hostname
-{
-    echo "127.0.0.1    localhost"
-    echo "::1          localhost"
-    echo "127.0.1.1    $hsn.localdomain    $hsn"
-} >> /etc/hosts
-echo "Enter new password for root"
+printf "%s" "$hsn" >> /etc/hostname
+printf "127.0.0.1    localhost\n::1          localhost\n127.0.1.1    %s.localdomain    %s" "$hsn" "$hsn" >> /etc/hosts
+printf "Enter new password for root"
 passwd
 
-# Install some packages
+# install some packages
 pacman -S --noconfirm grub efibootmgr networkmanager mtools dosfstools ntfs-3g \
-    ufw dash pipewire pipewire-pulse pipewire-jack neovim wget man-db \
-    linux-headers reflector
+    ufw dash pipewire pipewire-alsa pipewire-pulse pipewire-jack linux-headers
 
-# Relink dash to /bin/sh
+# relink dash to /bin/sh and create hook to relink dash to /bin/sh everytime bash gets updated
 ln -sfT dash /usr/bin/sh
 printf "[Trigger]\nType = Package\nOperation = Install\nOperation = Upgrade\nTarget = bash\n\n[Action]\nDescription = Re-pointing /bin/sh symlink to dash...\nWhen = PostTransaction\nExec = /usr/bin/ln -sfT dash /usr/bin/sh\nDepends = dash" > /usr/share/libalpm/hooks/binsh2dash.hook
 
-# Install grub
-sed -i '4s/5/-1/' /etc/default/grub
+# install grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=-1/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Enable services
+# enable services
 systemctl enable NetworkManager
 systemctl enable fstrim.timer
 systemctl enable reflector.timer
 systemctl enable ufw.service
 
-# Add user
+# add user
 read -p "Enter desired username: " usn
 useradd -m "$usn"
-echo "Enter password for $usn"
+printf "Enter password for %s" "$usn"
 passwd "$usn"
-usermod -a -G wheel mai
+usermod -a -G wheel "$usn"
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
-# Done
-echo "All done. You can now exit, unmount, and reboot :)"
+# prompt if user wants to use my personal postinstall script
+read -p "Would you like to use my personal postinstall script after restarting?(y/n): " ans
+if [[ $ans = y ]]; then
+    mkdir -p /home/"$usn"/files/repos
+    mv /tmp/my-arch-install /home/"$usn"/files/repos/.
+    chown -R "$usn":"$usn" /home/"$usn"/files
+    printf "You answered Yes. Run \"cd ~/files/repos/my-arch-install && ./2-postinstall.sh\" after rebooting."
+elif [[ $ans = n ]]; then
+    printf "You answered No."
+else
+    printf "Answered neither. Assuming your answer is No."
+fi
+
+# done
+printf "Base installation done! Run \"exit\", then \"umount -R /mnt\", and \"reboot now\" :)"
