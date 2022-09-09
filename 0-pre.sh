d999bcb939f37a0b74d4fcf57d75f6c20f440b1a
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2022-09-10 06:25:01 +0800

    Made a lot of changes to the script

--- 0-pre.sh
+++ 0-pre.sh
@@ -19,16 +19,26 @@ gdisk "$dr"
 # partition formatting
 clear
 lsblk
-read -p "Enter your EFI partition(Ex. \"/dev/sda1\"): " ep
+read -p "Enter your boot partition(Ex. \"/dev/sda1\"): " ep
 read -p "Enter your root partition(Ex. \"/dev/sda2\"): " rp
 mkfs.fat -F 32 "$ep"
-mkfs.ext4 "$rp"
-mount "$rp" /mnt
-mkdir -p /mnt/boot/efi
-mount "$ep" /mnt/boot/efi
+cryptsetup -y -v luksFormat "$rp"
+cryptsetup open "$rp" crypt-root
+mkfs.ext4 /dev/mapper/crypt-root
+mount /dev/mapper/crypt-root /mnt
+mkdir /mnt/boot
+mount "$ep" /mnt/boot
+
+# Check if processor is AMD or Intel
+cpu=$(grep vendor_id /proc/cpuinfo)
+if [[ "$cpu" ==  *"AuthenticAMD"* ]]; then
+    microcode=amd-ucode
+else
+    microcode=intel-ucode
+fi
 
 # install essential packages
-pacstrap /mnt base base-devel linux linux-firmware amd-ucode
+pacstrap /mnt base base-devel linux linux-firmware "$microcode"
 
 # generate fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -37,7 +47,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 # copy base install script to /mnt and execute it
-sed '1,/^# BASE$/d' 0-pre.sh > /mnt/1-base.sh
+sed '1,/^# II. BASE$/d' 0-pre.sh > /mnt/1-base.sh
 chmod +x /mnt/1-base.sh
 arch-chroot /mnt ./1-base.sh
 exit
@@ -51,18 +61,20 @@ printf "Pre-installation done! Performing Base install now..."
 
 # enable and set ParallelDownloads to 15 and enable multilib repositories
 sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 15/' /etc/pacman.conf
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # use all cores for compilation and compression
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $(nproc) -z -)/g" /etc/makepkg.conf
 
-# make a 2GB swapfile and set swappiness to 1
-dd if=/dev/zero of=/etc/swapfile bs=1M count=2048 status=progress
-chmod 600 /etc/swapfile
-mkswap /etc/swapfile
-swapon /etc/swapfile
-printf "/etc/swapfile none swap defaults 0 0" >> /etc/fstab
+# make an 8GB encryoted swapfile and set swappiness to 1
+cd /opt
+dd if=/dev/zero of=swap bs=1M count=8192 status=progress
+cryptsetup --type plain -d /dev/urandom open swap swap
+chmod 600 swap
+mkswap swap
+swapon swap
+printf "swap /opt/swap /dev/urandom swap" >> /etc/crypttab
+printf "/dev/mapper/swap none swap sw 0 0" >> /etc/fstab
 printf "vm.swappiness=1" >> /etc/sysctl.d/99-swappiness.conf
 
 # set locale, hostname and hosts, and set root password
@@ -78,26 +90,27 @@ printf "Enter new password for root\n"
 passwd
 
 # install some packages
-pacman -Sy --noconfirm grub efibootmgr networkmanager mtools dosfstools ntfs-3g \
+pacman -S --noconfirm grub efibootmgr networkmanager mtools dosfstools ntfs-3g \
     ufw dash pipewire pipewire-alsa pipewire-pulse pipewire-jack linux-headers \
     reflector git wget neovim man-db polkit
+
+# open mkinitcpio.conf
+sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard modconf block encrypt filesystems fsck)/" /etc/mkinitcpio.conf
+mkinitcpio -p linux
 
 # relink dash to /bin/sh and create hook to relink dash to /bin/sh everytime bash gets updated
 ln -sfT dash /usr/bin/sh
 printf "[Trigger]\nType = Package\nOperation = Install\nOperation = Upgrade\nTarget = bash\n\n[Action]\nDescription = Re-pointing /bin/sh symlink to dash...\nWhen = PostTransaction\nExec = /usr/bin/ln -sfT dash /usr/bin/sh\nDepends = dash" > /usr/share/libalpm/hooks/binsh2dash.hook
 
-# prompt if dual-booting with windows
-read -p "Dual booting with windows?(y/n): " db
-case "$db" in
-    y|Y) printf "Dual booting, installing os-prober and setting GRUB_DISABLE_OS_PROBER to false..."
-        pacman -Sy --noconfirm os-prober
-        sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub ;;
-    *) printf "Not dual booting, proceeding with GRUB install..." ;;
-esac
-
 # remove grub timeout and install grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=-1/' /etc/default/grub
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# run blkid and output file to /tmp for reference to be used in setting kernel parameter
+blkid > /tmp/blkid.txt
+printf "cryptdevice=UUID=device-UUID:crypt-root root=/dev/mapper/crypt-root" >> /tmp/blkid.txt
+nvim /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # enable services
@@ -122,7 +135,7 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 read -p "Would you like to use my personal postinstall script after restarting?(y/n): " ans
 case "$ans" in
     y|Y) mkdir -p /home/"$usn"/files/repos
-        sed '1,/^# POSTINSTALLATION$/d' 1-base.sh > /home/"$usn"/files/repos/2-post.sh
+        sed '1,/^# III. POSTINSTALLATION$/d' 1-base.sh > /home/"$usn"/files/repos/2-post.sh
         chown -R "$usn":"$usn" /home/"$usn"/files
         printf "You answered Yes. Run \"cd ~/files/repos/ && ./2-postinstall.sh\" after rebooting."
         exit ;;
@@ -131,7 +144,7 @@ case "$ans" in
         exit ;;
 esac
 
-# POSTINSTALLATION
+# III. POSTINSTALLATION
 #!/bin/sh
 
 # enable ufw
@@ -141,13 +154,15 @@ sudo ufw enable
 mkdir ~/.config
 mkdir -p ~/.local/src ~/.local/share
 cd ~/.local/share && mkdir cargo go wallpapers 
-cd ~/files && mkdir desktop documents downloads games music pictures public templates videos
+cd ~/files && mkdir desktop documents downloads music pictures public templates videos
 
 # make mount directories, mount flashdrive and copy files
-cd /mnt && sudo mkdir usb
+cd /mnt && sudo mkdir usb hdd
 sudo chown mai: usb
 sudo chmod 750 usb
-sudo mount /dev/sdc1 /mnt/usb
+sudo chown mai: hdd
+sudo chmod 750 hdd
+sudo mount /dev/sda1 /mnt/usb
 sudo cp /mnt/usb/.a/navi /etc/navi
 cp /mnt/usb/yes-man.jpg ~/.local/share/wallpapers/yes-man.jpg
 ln -s ~/.local/share/wallpapers/yes-man.jpg ~/.local/share/bg
@@ -179,14 +194,14 @@ ln -s ~/files/repos/dotfiles/local/bin ~/.local/bin
 ln -s ~/.config/shell/profile ~/.zprofile
 
 # install vim-plug for neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install packages I use
-sudo pacman -Sy --noconfirm xorg-server xorg-xinit xorg-xev libnotify mpd mpv \
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xev libnotify mpd mpv \
     ncmpcpp unclutter sxiv libreoffice-fresh dunst gimp lxappearance htop bc \
     keepassxc pcmanfm zathura zathura-pdf-mupdf zathura-cb scrot obs-studio \
     pulsemixer jdk-openjdk jre-openjdk jre-openjdk-headless xwallpaper p7zip \
-    unzip unrar rust go ttf-liberation ttf-nerd-fonts-symbols ueberzug zsh \
+    unzip unrar rust go ttf-liberation ttf-nerd-fonts-symbols-2048-em-mono ueberzug zsh \
     zsh-syntax-highlighting ffmpegthumbnailer highlight odt2txt file-roller \
     catdoc docx2txt perl-image-exiftool python-pdftotext android-tools xclip \
     noto-fonts-emoji noto-fonts-cjk arc-icon-theme firefox fzf alacritty \
@@ -196,7 +211,7 @@ sudo pacman -Sy --noconfirm xorg-server xorg-xinit xorg-xev libnotify mpd mpv \
 git clone https://aur.archlinux.org/paru.git ~/.local/src/paru
 cd ~/.local/src/paru || exit
 makepkg -si
-sudo sed -i '17s/.//' /etc/paru.conf
+sudo sed -i "s/#BottomUp/BottomUp/" /etc/paru.conf
 paru freetube-bin
 paru gtk-theme-arc-gruvbox-git
 paru lf-git
@@ -205,8 +220,13 @@ sudo sed -i "/\[bin\]/,/FileManager = vifm/"'s/^#//' /etc/paru.conf
 sudo sed -i 's/vifm/lfrun/' /etc/paru.conf
 
 # install my suckless tools
-mkdir ~/files/repos && cd ~/files/repos || exit
-cp -r /mnt/usb/suckless .
+cd ~/files/repos || exit
+git clone https://github.com/DefinitelyNotMai/dmenu
+git clone https://github.com/DefinitelyNotMai/dwm
+git clone https://github.com/DefinitelyNotMai/scroll
+git clone https://github.com/DefinitelyNotMai/slock
+git clone https://github.com/DefinitelyNotMai/slstatus
+git clone https://github.com/DefinitelyNotMai/st
 cd dmenu && sudo make install
 cd ../dwm && sudo make install
 cd ../scroll && sudo make install
