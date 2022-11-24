5caa9c4c2c02baae25554d51a12ecef3d8beb02d
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2022-11-25 01:46:17 +0800

    Removed some npm packages

--- 0-pre.sh
+++ 0-pre.sh
@@ -357,7 +357,7 @@ sudo usermod -aG libvirt,kvm $(whoami)
 chsh -s /usr/bin/zsh
 
 # echo out an npm installation script for neovim config to be ran after restart
-printf "#!/bin/sh\n\nnpm i -g typescript typescript-language-server vscode-langservers-extracted vls @tailwindcss/language-server yaml-language-server @prisma/language-server emmet-ls neovim graphql-language-service-cli @astrojs/language-server prettier" > ~/final.sh && chmod +x final.sh
+printf "#!/bin/sh\n\nnpm i -g typescript typescript-language-server vscode-langservers-extracted vls @tailwindcss/language-server yaml-language-server emmet-ls neovim graphql-language-service-cli @astrojs/language-server prettier" > ~/final.sh
 
 # done
 clear
